498c22b9fa8adab8dcb4e57c4400d8c7123e3b3a
ekansh verma <21bee083@nith.ac.in>
2023-04-12 11:34:30 +0530

    added structure

---
+++ src/components/Aboutpage/Aboutpage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../../pages/style.css';
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+function Aboutpage() {
+    return (
+        <div className='aboutpagebackground'>
+            <Container>
+                <Row className='textbackground'>
+                    <Col md={7} >
+                        <h3 className='aboutmetext'>About <span>Me</span></h3>
+                        <p className='aboutdetails'>I'm a self-taught Front End developer and Web designer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. I am also into web designing and grapgic designing. A responsive design makes your website accessible to all users, regardless of their devices.
+                        </p>
+                        <ul className='skilllist'>
+                            <Row>
+                                <h3>Skills</h3>
+                                <Col md={7}>
+                                    <li>HTML5/CSS3</li>
+                                    <li>JavaScript</li>
+                                    <li>React JS</li>
+                                    <li>Bootsrap 5</li>
+                                </Col>
+                                <Col md={5}>
+                                    <li>Adobe Illustrator</li>
+                                    <li>Adobe Photoshop</li>
+                                    <li>Figma</li>
+                                    <li>Python</li>
+                                    <li>Git/Github</li>
+                                </Col>
+                            </Row>
+                        </ul>
+                    </Col>
+                    <Col md={5}>
+                        <div className="webimage"></div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Aboutpage
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../../pages/style.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import Text from '../Homepage/Text'
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home() {
+  return (
+    <div className='homepagebackground'>
+      <Container>
+        <Row>
+          <Col md={7}>
+            <h2 className='headtext'>Hello <span className='wave'>👋 </span></h2>
+            <h2 className='nametext'>I'm Ekansh Verma</h2>
+            <span></span>
+            <Text />
+            <button onClick={() => {
+              window.open("https://github.com/ekanshverma143");
+            }}
+              className='socailmediabtn'><AiFillGithub className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://www.linkedin.com/in/ekansh-verma-469557228/");
+            }}
+              className='socailmediabtn'><FaLinkedinIn className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://twitter.com/i_ekanshverma");
+            }}
+              className='socailmediabtn'><AiOutlineTwitter className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://instagram.com/i_ekanshverma");
+            }}
+              className='socailmediabtn'><AiFillInstagram className='icon' /></button>
+          </Col>
+
+          <Col md={5}>
+            <div className="imagedeveloper">
+            </div>
+          </Col>
+        </Row>
+        <hr className='line' />
+        <p className='copyright'>© Copyright 2023.
+        <br /> 
+        Designed & Built by <span>Ekansh Verma</span></p>
+      </Container>
+    </div>
+  )
+}
+
+export default Home
